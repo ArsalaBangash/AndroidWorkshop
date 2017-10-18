0603926dc8fe5b938d5811a4ca0c29f322622bcd
ArsalaBangash <arsalabangash@gmail.com>
2017-10-18 09:46:43 -0400

    Commented code

--- app/src/main/java/com/example/arsala/todolist/MainActivity.java
+++ app/src/main/java/com/example/arsala/todolist/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.arsala.todolist;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.BaseAdapter;
 import android.widget.EditText;
@@ -14,9 +13,8 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    //Declare all view and model variables
     private EditText todotext;
-    private CardView todoCard;
     private FloatingActionButton addTodoButton;
     private ArrayList<String> todos;
     private BaseAdapter todoListAdapter;
@@ -26,14 +24,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind all views to their xml layout items
         todotext = (EditText) findViewById(R.id.todo_text);
         addTodoButton = (FloatingActionButton) findViewById(R.id.add_todo_button);
         todoList = (ListView) findViewById(R.id.todo_list);
+
+        //Initialize the model and the list adapter, and bind the adapter to the list
         todos = new ArrayList<String>();
         todoListAdapter = new ToDoListAdapter(this, todos);
         todoList.setAdapter(todoListAdapter);
 
 
+        /*
+        Upon the click of the FloatingActionButton, we want to change the model and notify the
+        adapter of this change so that it can let the list know what items to render
+         */
         addTodoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/example/arsala/todolist/ToDoListAdapter.java
+++ app/src/main/java/com/example/arsala/todolist/ToDoListAdapter.java
@@ -11,11 +11,19 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
+/**
+ * This class allows us to conveniently bind out model (in this case an array of strings) to the
+ * listview by overriding some methods inherited from the BaseAdapter class.
+ *
+ * These methods define how the adapter should react to changes in the model and how it should pass
+ * those changes on to the view...which in this case is our listview of cards.
+ */
 public class ToDoListAdapter extends BaseAdapter {
 
     private ArrayList<String> todos;
     private Context context;
 
+    //The constructor needs to context so that it can inflate the views for each row
     public ToDoListAdapter(Context context, ArrayList<String> todos) {
         this.context = context;
         this.todos = todos;
@@ -38,7 +46,14 @@ public class ToDoListAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
+        //Creates a card view from the layout resource file.
         View todoCardView = LayoutInflater.from(context).inflate(R.layout.todo_card, null);
+
+        /*
+        The int i refers to the position index within the listview that we want to render.
+        We use this to also index our model so that we know which item within the todos model we
+        want to pass into the TextView of the ToDoCard
+         */
         String currentTodo = todos.get(i);
         TextView todoCardText = (TextView) todoCardView.findViewById(R.id.todo_card_text);
         todoCardText.setText(currentTodo);
